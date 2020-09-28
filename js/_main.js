606af76d3bfb89d17154ba3b1225c05cc770a0ae
Nicola-9 <Nicola-9@users.noreply.github.com>
2020-09-28 17:55:52 +0200

    Add more explanation comments to code

--- js/_main.js
+++ js/_main.js
@@ -1,29 +1,34 @@
 $(document).ready(() =>{
+    // Select the buttons to add a todo in first list and to add a new card
     let addNewCardBtn = $('.add-card-btn');
     let addTodoFirstBtn = $('.add-todo-btn-1');
 
+    // Set Listener
     addNewCardBtn.on('click', () => {
         addNewCard();
     });
 
+    // Verify enter press after the list name is written
     $('.input-list-name-1').on('keypress', (event) =>{
         if(event.which == 13){
             $('.input-list-name-1').prop('disabled', true);
         }
     });
 
+    // Set listener to add todo in first card
     addTodoFirstBtn.click(() =>{
         let todoIndex = $('.todo-list-1').children().length + 1;
-
-        console.log(todoIndex);
 
         addNewTodo(1, todoIndex);
     });
 });
 
+// Function to add a new todos card
 function addNewCard(){
+    // Get number of cards in the page
     let index = $('.row').children().length + 1;
 
+    // Create new card (Never write innerHTML, XSS is near to us, Fortunatly this code isn't online)
     let newCard = '<article class="col-12 col-sm-12 col-md-3 col-lg-3 px-1 mb-2">' +
                         '<div class="card card-' + index + '">' +
                             '<h5 class="card-header">' + 
@@ -44,21 +49,24 @@ function addNewCard(){
                         '</div>' +
                     '</article>';
 
+    // Remove add card button
     let oldNewCardButton = $('.add-card-btn-section').remove();
 
+    // Select row that contains the todos cards
     let cardRow = $('.row');
 
+    // Append new card and re-insert the button to add new cards
     cardRow.append(newCard);
     cardRow.append(oldNewCardButton);
 
+    // Set listener on add card button
     $('.add-card-btn').on('click', () => {
         let cardNumber = $('.row').children().length - 1;
-
-        console.log(cardNumber);
 
         addNewCard(parseInt(cardNumber));
     });
 
+    // Set listener on header card title input on enter press to save the card title
     $('.input-list-name-' + index).on('keypress', (event) =>{
             if(event.which == 13){
                 $('.input-list-name-' + index).prop('disabled', true);
@@ -69,6 +77,7 @@ function addNewCard(){
             }
     });
 
+    // Set listener for todo button
     $('.add-todo-btn-' + index).click(() =>{
         let todoIndex = $('.todo-list-' + index).children().length + 1;
 
@@ -78,10 +87,13 @@ function addNewCard(){
     });
 }
 
+// Function to add new todos in every list
 function addNewTodo(index, indexTodo){
+    // Select the specific todos list by index and get todo text from input (Cut start and ending whitespace)
     let todoList = $('.todo-list-' + index);
     let todoText = $('.todo-input-' + index).val().trim();
 
+    // Create new Todo item (Attention, XSS vulnerability is over the thousand %)
     let newTodo = '<li class="todo todo-' + (+index) + "-" + (+indexTodo) + '">' +
                         '<div class="card card-todo card-todo-' + (+index) + "-" + (+indexTodo) +  '">' + 
                             '<div class="card-body card-body-todo card-text card-body-todo-' + (+index) + "-" + (+indexTodo) + '">' +
@@ -92,15 +104,20 @@ function addNewTodo(index, indexTodo){
                         '</div>' +
                   '</li>';
 
+    // Append new todo to the unordered list in the page
     todoList.append(newTodo);
 
+    // Set a little bit of CSS to todo card
     $('.card-body-todo-' + (+index) + "-" + (+indexTodo)).css({
         "background-color": "#fff",
         "font-size": "1.5rem !important",
     });
 
+    // Generate string index for a specific todo with notation x-x
     let indexNew = ((+index) + "-" + (+indexTodo)).toString();
 
+    // Set listener on check todo, if todo is checked, the todo item text will have a yellow color
+    // and check box will be checked and will have the same color of the text
     $('#unchecked-' + indexNew).click(() =>{
         $('#unchecked-' + (+index) + "-" + (+indexTodo)).css({
             "display": "none"
@@ -115,6 +132,8 @@ function addNewTodo(index, indexTodo){
         });
     });
 
+    // Set listener if todo item checked is clicked.
+    // The text will have a black color and the checkbox too.
     $('#checked-' + indexNew).click(() =>{
         $('#checked-' + (+index) + "-" + (+indexTodo)).css({
             "display": "none"
